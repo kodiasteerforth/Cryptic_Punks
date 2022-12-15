379d887984f7f0d21a0c92804e95f088ff08d823
kodiasteerforth <kodiasteerforth@gmail.com>
2022-12-15 10:52:37 -0600

    Deployed Contracts

---
+++ src/components/Navbar.js
@@ -0,0 +1,29 @@
+const Navbar = ({ web3Handler, account, explorerURL }) => {
+    return (
+        <nav className="navbar fixed-top mx-3">
+            <a
+                className="navbar-brand col-sm-3 col-md-2 mr-0 mx-4"
+                href="http://www.dappuniversity.com/bootcamp"
+                target="_blank"
+                rel="noopener noreferrer"
+            >
+
+                Conscious Cryptic Network
+            </a>
+
+            {account ? (
+                <a
+                    href={`${explorerURL}/address/${account}`}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="button nav-button btn-sm mx-4">
+                    {account.slice(0, 5) + '...' + account.slice(38, 42)}
+                </a>
+            ) : (
+                <button onClick={web3Handler} className="button nav-button btn-sm mx-4">Connect Wallet</button>
+            )}
+        </nav>
+    )
+}
+
+export default Navbar;
